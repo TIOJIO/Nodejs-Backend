6d9c64b962ee79ae0057075d7412bd3a1d2320e1
tiojio <tiojioromain02@gmail.com>
2023-10-17 04:41:57 +0100

    add middleWare

---
+++ BackEnd/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./App')
+
+app.set('port',process.env.PORT || 3000);
+const server = http.createServer(app);
+
+
+server.listen(process.env.PORT || 3000);
